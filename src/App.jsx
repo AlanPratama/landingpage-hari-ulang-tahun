a6a94d1cbea8acdad90bf4f996812e2bb76d4370
AlanPratama <pralan76@gmail.com>
2024-08-19 10:36:36 +0700

    [UI]: make navbar and layouts components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
+import { RiLayout } from './layouts/RiLayout';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<RiLayout><Home /></RiLayout>} />
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
