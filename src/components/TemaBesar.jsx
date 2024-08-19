3e5aaed77bf02a4e8f5cb2e4929cf415fd357b7d
fauzangan <fauzan12356@gmail.com>
2024-08-19 13:55:19 +0700

    add motion

--- src/components/TemaBesar.jsx
+++ src/components/TemaBesar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
+import { motion } from 'framer-motion';
 
 function TemaBesar({ slides }) {
     const settings = {
@@ -16,11 +17,19 @@ function TemaBesar({ slides }) {
                 <div> 
                     <div key={index} className="flex flex-row items-center justify-center w-full p-4 font-lato">
                         <div className="w-1/2 flex-shrink-0">
-                            <img src={slide.imageSrc} alt="background" className="w-full h-auto border border-gray-300 rounded-lg shadow-md" />
+                            <img src={slide.imageSrc} alt="background" className="w-full h-auto border border-gray-300 rounded-lg shadow-lg" />
                         </div>
                         <div className="w-1/3 text-left pl-8 flex flex-col">
-                            <h1 className="text-red-600 text-4xl font-bold">{slide.title}</h1>
-                            <h2 className="text-black text-2xl pt-5">{slide.subtitle}</h2>
+                            <motion.h1 className="text-red-600 text-4xl font-bold"
+                                initial={{ opacity: 0, y: 20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 1 }}
+                            >{slide.title}</motion.h1>
+                            <motion.h2 className="text-black text-2xl pt-5"
+                                initial={{ opacity: 0, y: 20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 1, delay: 0.2 }}
+                            >{slide.subtitle}</motion.h2>
                         </div>
                     </div>
                 </div>
