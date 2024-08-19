2a2c05ed680a7c9c639a372080b7996bcc742103
amalkhairin <amallanch45@gmail.com>
2024-08-19 23:42:53 +0700

    fix bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -208,7 +208,7 @@ export const Home = () => {
           <h1 className="text-2xl sm:text-3xl font-bold capitalize mb-2">"{quotes.quote}"</h1>
           <p className="text-xl font-semibold">- {quotes.name}</p>
 
-          <button onClick={handleGenerateQuote} type="button" class="mt-4 text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Generate Quote</button>
+          <button onClick={handleGenerateQuote} type="button" className="mt-4 text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Generate Quote</button>
         </div>
       </div>
       )}
