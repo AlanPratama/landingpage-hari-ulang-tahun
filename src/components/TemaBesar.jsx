ff0ccd48155dae80f38da2db90b56c0337705605
fauzangan <fauzan12356@gmail.com>
2024-08-19 17:22:19 +0700

    fix slide

--- src/components/TemaBesar.jsx
+++ src/components/TemaBesar.jsx
@@ -8,7 +8,6 @@ function TemaBesar({ slides }) {
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        fade: true,
         slidesToScroll: 1,
         waitForAnimate: false
     };
@@ -20,7 +19,7 @@ function TemaBesar({ slides }) {
                     <div key={index} className='mx-0'> 
                         <div className="flex flex-row items-center justify-center w-auto mx-0 font-lato">
                             <div className="w-1/2 flex-shrink-0 mx-0">
-                                <img src={slide.imageSrc} alt="background" className="w-auto h-auto border border-gray-300 rounded-lg shadow-lg mx-0" />
+                                <img src={slide.imageSrc} alt="background" className="w-auto border border-gray-300 rounded-lg shadow-lg mx-0" />
                             </div>
                             <div className="w-1/3 text-left pl-8 flex flex-col">
                                 <motion.h1 className="text-red-600 text-4xl font-bold"
